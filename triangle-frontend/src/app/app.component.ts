4a4397afee2692bbfc70ed61fa1efb482d77abd3
alexfargo <kevin.a.fargo@gmail.com>
2025-04-13 20:26:58 -0600

    test conflict

--- triangle-frontend/src/app/app.component.ts
+++ triangle-frontend/src/app/app.component.ts
@@ -19,6 +19,8 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {}
 
+// stuff!
+
   getTriangleType() {
     const url = `https://triangle-middleware-app-production.up.railway.app/triangle/type?side1=${this.side1}&side2=${this.side2}&side3=${this.side3}`;
     this.http.get<{ type: string }>(url).subscribe({
