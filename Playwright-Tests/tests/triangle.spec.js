977eefb04a5f7de2c124ec87c9f3e5585b78d5c3
Alex Fargo <kevin.a.fargo@gmail.com>
2025-04-24 20:05:49 -0600

    Playwright2 (#12)
    * added playwright
    
    * more tests

--- Playwright-Tests/tests/triangle.spec.js
+++ Playwright-Tests/tests/triangle.spec.js
@@ -8,7 +8,7 @@ test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/TriangleFrontend/);
 });
 
-test("test 2", async ({ page }) => {
+test("get equilateral", async ({ page }) => {
   await page.goto("https://trianglefrontend-production.up.railway.app/");
 
   await page.getByRole("spinbutton", { name: "Side 1" }).click();
@@ -18,18 +18,26 @@ test("test 2", async ({ page }) => {
   await page.getByRole("spinbutton", { name: "Side 3" }).click();
   await page.getByRole("spinbutton", { name: "Side 3" }).fill("3");
   await page.getByRole("button", { name: "Get Triangle Type" }).click();
-  await expect(page.locator("*")).toContainText(
-    "Type of Triangle: Equilateral"
-  );
+  await expect(page.getByText("Type of Triangle: Equilateral")).toBeVisible();
   //await page.pause();
-  // Expect a title "to contain" a substring.
 });
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
+test("get scalene", async ({ page }) => {
+  await page.goto("https://trianglefrontend-production.up.railway.app/");
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+  await page.getByRole("spinbutton", { name: "Side 1" }).click();
+  await page.getByRole("spinbutton", { name: "Side 1" }).fill("4");
+  await page.getByRole("spinbutton", { name: "Side 2" }).click();
+  await page.getByRole("spinbutton", { name: "Side 2" }).fill("3");
+  await page.getByRole("spinbutton", { name: "Side 3" }).click();
+  await page.getByRole("spinbutton", { name: "Side 3" }).fill("5");
+  await page.getByRole("button", { name: "Get Triangle Type" }).click();
+  await expect(page.getByText("Type of Triangle: Scalene")).toBeVisible();
+  // await page.pause();
+});
+
+test("get isosolies", async ({ page }) => {
+  await page.goto("https://trianglefrontend-production.up.railway.app/");
+
+  await page.pause();
+});
