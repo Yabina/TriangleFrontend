c13c8321b3c012f13c4508e156a54970db070ae6
Yabina <82197032+Yabina@users.noreply.github.com>
2025-04-24 23:37:48 -0400

    Playwright Tests (#13)
    * added playwright
    
    * more tests

--- Playwright-Tests/tests/triangle.spec.js
+++ Playwright-Tests/tests/triangle.spec.js
@@ -6,16 +6,14 @@ test("has title", async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/TriangleFrontend/);
+  await expect(page.getByText("MSSE640 Frontend Angular App")).toBeVisible();
+   //await page.pause();
 });
 
 test("get equilateral", async ({ page }) => {
   await page.goto("https://trianglefrontend-production.up.railway.app/");
-
-  await page.getByRole("spinbutton", { name: "Side 1" }).click();
   await page.getByRole("spinbutton", { name: "Side 1" }).fill("3");
-  await page.getByRole("spinbutton", { name: "Side 2" }).click();
   await page.getByRole("spinbutton", { name: "Side 2" }).fill("3");
-  await page.getByRole("spinbutton", { name: "Side 3" }).click();
   await page.getByRole("spinbutton", { name: "Side 3" }).fill("3");
   await page.getByRole("button", { name: "Get Triangle Type" }).click();
   await expect(page.getByText("Type of Triangle: Equilateral")).toBeVisible();
@@ -24,7 +22,6 @@ test("get equilateral", async ({ page }) => {
 
 test("get scalene", async ({ page }) => {
   await page.goto("https://trianglefrontend-production.up.railway.app/");
-
   await page.getByRole("spinbutton", { name: "Side 1" }).click();
   await page.getByRole("spinbutton", { name: "Side 1" }).fill("4");
   await page.getByRole("spinbutton", { name: "Side 2" }).click();
@@ -38,6 +35,26 @@ test("get scalene", async ({ page }) => {
 
 test("get isosolies", async ({ page }) => {
   await page.goto("https://trianglefrontend-production.up.railway.app/");
+  await page.getByRole('spinbutton', { name: 'Side 1' }).fill('3');
+  await page.getByRole('spinbutton', { name: 'Side 2' }).fill('3');
+  await page.getByRole('spinbutton', { name: 'Side 3' }).fill('5');
+  await page.getByRole('button', { name: 'Get Triangle Type' }).click();
+  await expect(page.getByText("Type of Triangle: Isosceles")).toBeVisible();
+ // await page.pause();
+});
 
-  await page.pause();
+test("has footer", async ({ page }) => {
+  await page.goto("https://trianglefrontend-production.up.railway.app/");
+  await page.getByText('© 2025 TriangleFrontend').click();
+  await expect(page.getByText('© 2025 TriangleFrontend')).toBeVisible();
+ //await page.pause();
+});
+
+test("has link", async ({ page }) => {
+  await page.goto("https://trianglefrontend-production.up.railway.app/");
+  await page.getByRole('link', { name: 'View on GitHub' }).click();
+  await expect(page).toHaveURL(new RegExp("https://trianglefrontend-production.up.railway.app/"));
+  await expect(page.getByText('TriangleFrontend')).toBeVisible();
+ 
+ await page.pause();
 });
